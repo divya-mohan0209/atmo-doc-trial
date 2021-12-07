175cb4c73ed04c088aef0e1f3f754e831364d751
divya-mohan0209 <divya.mohan0209@gmail.com>
2021-12-07 19:05:04 +0530

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -3,6 +3,7 @@ set -e
 
 INPUT_BRANCH=${INPUT_BRANCH:-gh-pages}
 INPUT_DIRECTORY=${INPUT_DIRECTORY:-'website/build'}
+OUTPUT_DIRECTORY=${OUTPUT_DIRECTORY:-'gh-pages'}
 
 echo "Push to branch $INPUT_BRANCH";
 [ -z "${GITHUB_TOKEN}" ] && {
@@ -10,15 +11,21 @@ echo "Push to branch $INPUT_BRANCH";
     exit 1;
 };
 
-cp -r ${INPUT_DIRECTORY} /gh-pages
+remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
-cd /gh-pages
-git init
+rm -rf "${OUTPUT_DIRECTORY}"
+git clone --branch "${INPUT_BRANCH}" "${remote_repo}" "${OUTPUT_DIRECTORY}"
+mv "${OUTPUT_DIRECTORY}/.git" output.git
+rm -rf "${OUTPUT_DIRECTORY}"
+cp -r "${INPUT_DIRECTORY}" "${OUTPUT_DIRECTORY}"
+
+mv output.git "${OUTPUT_DIRECTORY}/.git"
+cd "${OUTPUT_DIRECTORY}"
+touch .nojekyll
+
 git config --local user.email "action@github.com"
 git config --local user.name "GitHub Action"
 git add .
 git commit -m "Push to Github Pages"
-
-remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
 
 git push "${remote_repo}" HEAD:${INPUT_BRANCH} --follow-tags --force
